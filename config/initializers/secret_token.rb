9c7738ac6479257739ce71de789335faa8b57325
janusz.liro <janusz.liro@mobica.com>
2016-04-07 12:45:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MdmTest12::Application.config.secret_key_base = '1786542b935872e0c0bb3bb24c6a8a85a5bcc80135d7bd28cb9ac46e94f14941dac39007d1883cb0cbc29e4a5dfc409f1532d39273b589bdbd14e8933c179cd1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MdmTest12::Application.config.session_store :cookie_store, key: '_mdm_test12_session'
